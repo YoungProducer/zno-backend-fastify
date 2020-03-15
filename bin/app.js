24f5b62bc14a822219ab12c9c3cf30350bdac68f
YoungProducer <sashabezrukovownmail@gmail.com>
2020-03-15 17:06:22 +0200

    Changed host name from localhost to 0.0.0.0.

--- bin/app.js
+++ bin/app.js
@@ -11,7 +11,7 @@ const instancePath = mode === 'development'
 const app = require(instancePath).instance
 
 // Start listening on 3000 port
-app.listen(process.env.PORT || 4000, '127.0.0.1', (err) => {
+app.listen(process.env.PORT || 4000, '0.0.0.0', (err) => {
     if (err) throw err;
 
     console.log(`Application is ready and listening on http://localhost:${process.env.PORT}`);
