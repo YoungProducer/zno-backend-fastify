9441ac2d5a731453b51a0fd63a358e2fa9580b7a
YoungProducer <sashabezrukovownmail@gmail.com>
2020-03-16 00:49:48 +0200

    Changed docker script.

--- configs/deploy/prisma_build.sh
+++ configs/deploy/prisma_build.sh
@@ -5,7 +5,7 @@ set -e
 VERSION="${1:?Provide the Prisma version this script should use}"
 APP="${2:?Provide the Heroku app name to deploy/release to}"
 
-docker build --build-arg tag=${VERSION} -t registry.heroku.com/${APP}/prisma .
-docker push registry.heroku.com/${APP}/prisma
+docker build --build-arg tag=${VERSION} -t registry.heroku.com/${APP}/web .
+docker push registry.heroku.com/${APP}/web
 
 echo "Image pushed successfully. Release with heroku container:release web -a ${APP}"
