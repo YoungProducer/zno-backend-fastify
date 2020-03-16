b84ac5ceee265c95a5b9096bb2c1bab470421855
YoungProducer <sashabezrukovownmail@gmail.com>
2020-03-16 12:52:35 +0200

    Added fixes and changed prisma endpoint.

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ ARG tag
 FROM prismagraphql/prisma:$tag
 COPY ./prisma/prisma.yml /app/config.yml
 COPY ./configs/deploy/prerun_hook.sh /app/prerun_hook.sh
+RUN /bin/bash -c 'chmod a+x /app/prerun_hook.sh' 
 CMD /app/start.sh
--- configs/deploy/prerun_hook.sh
+++ configs/deploy/prerun_hook.sh
@@ -8,11 +8,13 @@ managementApiSecret: ${PRISMA_MANAGEMENT_API_SECRET}
 databases:
   default:
     connector: mongo
-    host: ${DB_HOST}
-    port: ${DB_PORT}
-    database: ${DB_NAME}
-    schema: public
-    user: ${DB_USER}
-    password: ${DB_PASSWORD}
-    migrations: true
+    # host: ${DB_HOST}
+    # port: ${DB_PORT}
+    # database: ${DB_NAME}
+    # schema: public
+    # user: ${DB_USER}
+    # password: ${DB_PASSWORD}
+    # migrations: true
+    # connector: mongo
+    uri: mongodb+srv://WithoutHands:Sasha080701@mycluster-qntjt.azure.mongodb.net/test?retryWrites=true&w=majority
 " >> ${PRISMA_CONFIG_PATH}
