07e2abd721879c21cab8059498e086d705b8867d
YoungProducer <sashabezrukovownmail@gmail.com>
2020-06-13 11:41:09 +0300

    feat: added subjectConfig model.

---
+++ lib/models/subjectConfig.ts
@@ -0,0 +1,53 @@
+/** External imports */
+import { Schema, Model, Document, model } from 'mongoose';
+
+export interface SubSubject {
+    subject: string;
+    themes: string[];
+}
+
+export interface Exams {
+    trainings: string[];
+    sessions: string[];
+}
+
+export interface SubjectConfigSchema {
+    _id: string;
+    subject: string;
+    themes: string[];
+    subSubjects: SubSubject[];
+    exams: Exams;
+}
+
+const subjectConfigSchema = new Schema<SubjectConfigSchema>({
+    subject: {
+        type: Schema.Types.ObjectId,
+        ref: 'Subject',
+    },
+    themes: [{
+        type: String,
+    }],
+    subSubjects: [{
+        subject: {
+            type: Schema.Types.ObjectId,
+            ref: 'Subject',
+        },
+        themes: [{
+            type: String,
+        }],
+    }],
+    exams: {
+        trainings: [{
+            type: String,
+        }],
+        sessions: [{
+            type: String,
+        }],
+    },
+}, {
+    autoIndex: true,
+    timestamps: true,
+});
+
+export const subjectConfigModel: Model<SubjectConfigSchema & Document> =
+    model('SubjectConfig', subjectConfigSchema);
