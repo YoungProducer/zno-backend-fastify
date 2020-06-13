25ae35ac9f568583a3795322fbe0f0294df79abc
YoungProducer <sashabezrukovownmail@gmail.com>
2020-06-13 11:24:38 +0300

    feat: added subject model.

---
+++ lib/models/subject.ts
@@ -0,0 +1,38 @@
+/** External imports */
+import { Schema, Model, Document, model } from 'mongoose';
+
+export interface SubjectSchema {
+    _id: string;
+    name: string;
+    isSubSubject: boolean;
+    parent: string;
+    image: string;
+    icon: string;
+    config: string;
+}
+
+const subjectSchema = new Schema<SubjectSchema>({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    isSubSubject: {
+        type: Boolean,
+        required: false,
+        default: false,
+    },
+    parent: this,
+    image: String,
+    icon: String,
+    config: {
+        type: Schema.Types.ObjectId,
+        ref: 'SubjectConfig',
+    },
+}, {
+    autoIndex: true,
+    timestamps: true,
+});
+
+export const subjectModel: Model<SubjectSchema & Document> =
+    model('Subject', subjectSchema);
